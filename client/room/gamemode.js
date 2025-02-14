ac94fdf0971c0972bdfead91818b9dc97c953570
TɴTᴡ Oғғɪᴄɪᴀʟ - (GɪᴛHᴜʙ) PIXELCOMBATS 2 <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2025-02-15 02:08:53 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -13,16 +13,16 @@ const EndTriggerPoints = 10000; // Сколько даётся очков, за 
 const MaxSpawnsByArea = 25; // Максимальные спавны, на - зону.
 const LeaderBoardProp = 'Leader'; // Свойство, для - лидерБорда.
 const CurSpawnPropName = 'CurSpawn'; // Ствойство, индекса - 0 дефолтный, индекс.
-const EndAreaTag = 'EndAreasTag'; // Тег зоны, конца - паркура.
-const SpawnAreaTag = 'SpawnAreasTag'; // Тег зоны, промежуточного - сохранения.
+const ViewEndParameterName = 'ViewEnd'; // Название, визулизатор - конца паркура.
+const ViewSpawnsParameterName = 'ViewSpawns'; // Название, визулизатра - промежуточных спавнов.
+const EndAreaTag = 'EndAreaTag'; // Тег зоны, конца - паркура.
+const SpawnAreasTag = 'SpawnAreasTag'; // Тег зоны, промежуточного - сохранения.
 // Переменные:
 let MainTimer = Room.Timers.GetContext().Get('Main');
 let StateProp = Room.Properties.GetContext().Get('State');
 let EndAreas = Room.AreaService.GetByTag('EndAreasTag');
 let SpawnAreas = Room.AreaService.GetByTag('SpawnAreasTag');
 let Inventory = Room.Inventory.GetContext();
-let EndAreaColor = new Basic.Color(0, 0, 1, 0);
-let SpawnAreasColor = new Basic.Color(1, 1, 1, 0);
 
 // Свойства, параметр - режима:
 const MapRotation = Room.GameMode.Parameters.GetBool('MapRotation');
@@ -78,8 +78,68 @@ case EndOfMatchStateValue:
          }
 
 // Зона, конца - паркура:
-CreateNewArea('EndAreasTag', ['EndAreasTag'], true, function(p, a) {
- 
+CreateNewArea('EndAreaTag', ['EndAreasTag'], true, function(p, a) {
+ EndTrigger.Enable = false;
+p.Properties.Get('LeaderBoardProp').Value += EndTriggerPoints;
+  StateProp.Value = End0fMatchStateValue;
+}, function(p, a) {}, 'ViewEnd', new Basic.Color(0, 0, 1, 0));
+// Визулизатор, конца - паркура, с зоной - для цвета:
+if (Room.GameMode.Parameters.GetBool('ViewEndParameterName')) {
+CreateNewArea('EndAreaTag', ['EndAreasTag'], true, function(p, a) {
+p.Properties.Get('Scores').Value += EndTriggerPoints;
+  StateProp.Value = End0fMatchStateValue;
+ }, function(p, a) {}, 'ViewEnd', new Basic.Color(0, 0, 1, 0), true);
+}
+
+// Зона, промежуточных - спавнов/сохранений: 
+CreateNewArea('SpawnAreasTag', ['SpawnAreasTag'], true, function(p, a) {
+if (SpawnAreas == null || SpawnAreas.length == 0) InitializeMap(); // Todo костыль, баг остался.
+if (SpawnAreas == null || SpawnAreas.length == 0) return;
+const CurSpawn = p.Properties.Get(CurSpawnPropName);
+const LeaderBoardProp = p.Properties.Get(LeaderBoardProp);
+let I = 0;
+  if (CurSpawn.Value != null) I = CurSpawn.Value;
+	for (; I < SpawnAreas.length; ++I) {
+	   if (SpawnAreas[I] == a) {
+		if (CurSpawn.Value == null || I > CurSpawn.Value) {
+		CurSpawn.Value = I;
+	LeaderBoardProp.Value += 10;
+     } 
+  break;
+  }, }, }, function(p, a) {}, 'ViewSpawns', new Basic.Color(1, 1, 1, 0));
+ // Визуализатор, промежуточных - спавнов:
+CreateNewArea('SpawnAreasTag', ['SpawnAreasTag'], true, function(p, a) {
+if (SpawnAreas == null || SpawnAreas.length == 0) InitializeMap(); // Todo костыль, баг остался.
+if (SpawnAreas == null || SpawnAreas.length == 0) return;
+const CurSpawn = p.Properties.Get(CurSpawnPropName);
+const LeaderBoardProp = p.Properties.Get(LeaderBoardProp);
+let I = 0;
+  if (CurSpawn.Value != null) I = CurSpawn.Value;
+	for (; I < SpawnAreas.length; ++I) {
+	   if (SpawnAreas[I] == a) {
+		if (CurSpawn.Value == null || I > CurSpawn.Value) {
+		CurSpawn.Value = I;
+	LeaderBoardProp.Value += 10;
+     } 
+  break;
+  }, }, }, function(p, a) {}, 'ViewSpawns', new Basic.Color(1, 1, 1, 0), true);
+  
+// Настройка таймера, конца - игры:
+ MainTimer.OnTimer.Add(function() { StartVote(); });
+
+// ЛидерБорды:
+Room.LeaderBoard.PlayerLeaderBoardValues = [
+  new Basic.DisplayValueHeader('Deaths', '<b><size=30><color=#be5f1b>D</color><color=#b85519>e</color><color=#b24b17>a</color><color=#ac4115>t</color><color=#a63713>h</color><color=#a02d11>s</color></size></b>', '<b><size=30><color=#be5f1b>D</color><color=#b85519>e</color><color=#b24b17>a</color><color=#ac4115>t</color><color=#a63713>h</color><color=#a02d11>s</color></size></b>'),
+  new Basic.DisplayValueHeader('Spawns', '<b><size=30><color=#be5f1b>S</color><color=#b85519>p</color><color=#b24b17>a</color><color=#ac4115>w</color><color=#a63713>n</color><color=#a02d11>s</color></size></b>', '<b><size=30><color=#be5f1b>S</color><color=#b85519>p</color><color=#b24b17>a</color><color=#ac4115>w</color><color=#a63713>n</color><color=#a02d11>s</color></size></b>'),
+  new Basic.DisplayValueHeader('LeaderBoardProp', '<b><size=30><color=#be5f1b>S</color><color=#b85519>c</color><color=#b24b17>o</color><color=#ac4115>r</color><color=#a63713>e</color><color=#a02d11>s</color></size></b>', '<b><size=30><color=#be5f1b>S</color><color=#b85519>c</color><color=#b24b17>o</color><color=#ac4115>r</color><color=#a63713>e</color><color=#a02d11>s</color></size></b>'),
+];
+
+// 
+
+
+
+
+
 
 
 
