b0a0f54d5e400e31fec916c8123443130a4c3cc5
TɴTᴡ Oғғɪᴄɪᴀʟ - (GɪᴛHᴜʙ) PIXELCOMBATS 2 <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2025-02-15 01:30:20 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -5,7 +5,7 @@ import * as Room from 'pixel_combats/room';
 // Константы, таймера:
 const End0fMatchTime = 11;
 const GameModeTime = 381;
-const VoteTime = 15;
+const VoteTime = 16;
 // Константы:
 const EndOfMatchStateValue = 'EndOfMatch';
 const GameModeStateValue = 'Game';
@@ -13,12 +13,14 @@ const EndTriggerPoints = 10000; // Сколько даётся очков, за 
 const MaxSpawnsByArea = 25; // Максимальные спавны, на - зону.
 const LeaderBoardProp = 'Leader'; // Свойство, для - лидерБорда.
 const CurSpawnPropName = 'CurSpawn'; // Ствойство, индекса - 0 дефолтный, индекс.
+const EndAreaTag = 'EndAreasTag'; // Тег зоны, конца - паркура.
+const SpawnAreaTag = 'SpawnAreasTag'; // Тег зоны, промежуточного - сохранения.
 // Переменные:
 let MainTimer = Room.Timers.GetContext().Get('Main');
 let StateProp = Room.Properties.GetContext().Get('State');
-let EndAreas = Room.AreaService.GetByTag('EndAreaTag');
+let EndAreas = Room.AreaService.GetByTag('EndAreasTag');
 let SpawnAreas = Room.AreaService.GetByTag('SpawnAreasTag');
-let Inventory = Room.Inventory.GetContext(BlueTeam);
+let Inventory = Room.Inventory.GetContext();
 let EndAreaColor = new Basic.Color(0, 0, 1, 0);
 let SpawnAreasColor = new Basic.Color(1, 1, 1, 0);
 
@@ -33,8 +35,8 @@ Room.BreackGraph.WeakBlocks = true;
 Room.Teams.Add('Blue', '<b><size=30><color=#0d177c>ß</color><color=#03088c>l</color><color=#0607b0>ᴜ</color><color=#1621ae>E</color></size></b>', new Basic.Color(0, 0, 1, 0));
 let BlueTeam = Room.Teams.Get('BlueTeam');
 BlueTeam.Spawns.SpawnPointsGroups.Add(1);
-Room.Ui.GetContext(BlueTeam).Hint.Value = '!Пройдите, паркур - первым!';
-Room.Ui.GetContext(BlueTeam).MainTimerId.Value = MainTimer.Id;
+Room.Ui.GetContext().Hint.Value = '!Пройдите, паркур - первым!';
+Room.Ui.GetContext().MainTimerId.Value = MainTimer.Id;
 
 // Конфигурация, инвентаря:
 Inventory.Main.Value = false;
@@ -62,14 +64,11 @@ StateProp.OnValue.Add(OnState);
 function OnState() {
  switch (StateProp.Value) {
 case GameModeStateValue:
- Room.Spawns.GetContext(BlueTeam).Enable = true;
- Room.Spawns.GetContext(BlueTeam).Spawn();
-
-  MainTimer.Restart(GameModeTime);
+ Room.Spawns.GetContext().Enable = true;
  break;
 case EndOfMatchStateValue:
- Room.Spawns.GetContext(BlueTeam).Enable = false;
- Room.Spawns.GetContext(BlueTeam).Despawn();
+ Room.Spawns.GetContext().Enable = false;
+ Room.Spawns.GetContext().Despawn();
  Room.Game.GameOver(Room.LeaderBoard.GetPlayers());
 
   MainTimer.Restart(EndOfMatchTime);
@@ -79,7 +78,8 @@ case EndOfMatchStateValue:
          }
 
 // Зона, конца - паркура:
-CreateNewArea('
+CreateNewArea('EndAreasTag', ['EndAreasTag'], true, function(p, a) {
+ 
 
 
 
